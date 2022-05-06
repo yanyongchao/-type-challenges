79f19b0df05d519037e187efcade4e065bc9669a
yanyongchao <1585094062@qq.com>
2022-05-06 22:32:38 +0800

    TrimLeft

---
+++ TrimLeft.ts
@@ -0,0 +1,5 @@
+// 你的答案
+type trim = ' ' | '\n' | '\t'
+type TrimLeft<S extends string> = S extends `${trim}${infer L}` ? TrimLeft<L> : S
+
+type trimed = TrimLeft<'  Hello World  '> // 应推导出 'Hello World  '
